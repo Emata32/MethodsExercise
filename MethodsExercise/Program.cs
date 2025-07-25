53bf3eadd1adeb70983c559a2ec8012a61a7e7a9
Eduardo Mata <eddiemata15533@gmail.com>
2025-07-25 12:29:09 -0500

    Completed exercise 2 and challenge bonus.

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -15,10 +15,55 @@
             Console.WriteLine($"what kind of snacks dose your {kindOfPet} like?");
             String kindOfSnacks = Console.ReadLine();
         }
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+        
+        
 
         static void Main(string[] args)
         {
             Madlib();
+          
+           //ADD two numbers
+           Console.WriteLine(Add(1,1));
+           //Subtract two numbers
+           Console.WriteLine(Sub(8, 2));
+           //Multiply two numbers
+           Console.WriteLine(Multiply(1, 2));
+           //Divide two numbers
+           Console.WriteLine(Divide(4, 2));
+           Console.WriteLine(Sum(5, 5 , 5));
+           
+           
+           
         }
     }
 }
