5d22aefc6c99053a9675bee886f4dc41bd563bff
Eduardo Mata <eddiemata15533@gmail.com>
2025-07-25 11:46:56 -0500

    completed excercise one of methods excercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,24 @@
 {
     public class Program
     {
+        public static void Madlib()
+        {   Console.WriteLine("Hi nice to meet you whats you're name?");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine($"Hi {name}! What kind of pet do you have?");
+            string kindOfPet = Console.ReadLine();
+           
+            Console.WriteLine($"Wow a {kindOfPet}! What color is your {kindOfPet}?");
+            string colorOfPet = Console.ReadLine();
+            
+            Console.WriteLine($"what kind of snacks dose your {kindOfPet} like?");
+            String kindOfSnacks = Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Madlib();
         }
     }
 }
+
